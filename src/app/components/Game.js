cdbc6581d96bfdb142bc99c58176f2db4a6c202a
Mathys Dezitter <mathysdezitter.pro@gmail.com>
2024-12-03 14:07:31 +0100

    Create header & games display

--- src/app/components/Game.js
+++ src/app/components/Game.js
@@ -2,30 +2,35 @@ import Image from "next/image";
 
 export default function Game({ title, description, image, link }) {
     return (
-        <a href={`${link}`}>
-            <div className="p-2 max-w-sm">
-                <div className="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col min-w-80">
-                    <div className="relative h-40 w-full mb-5 rounded">
+        <a className="m-0 p-0" href={`${link}`}>
+            <div className="group m-2">
+                <div className="flex rounded-lg h-full bg-indigo-900 hover:bg-indigo-900 transition duration-300 flex-col min-w-48">
+                    {/* Image Container */}
+                    <div className="relative h-40 w-full rounded">
                         <Image
                             src={`/${image}`}
                             alt="logo"
                             fill
+                            className="rounded-t-lg group-hover:brightness-75 transition duration-300"
                             style={{ objectFit: 'cover' }}
                         />
                     </div>
-                    <div className="flex items-center mb-3">
-                        <div
-                            className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dice-5-fill" viewBox="0 0 16 16">
-                                <path d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3zm2.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M12 13.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M8 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
-                            </svg>
+                    {/* Content */}
+                    <div className="bg-indigo-500 group-hover:bg-indigo-600 rounded-b-lg p-8 transition duration-300">
+                        <div className="flex items-center mb-3">
+                            <div
+                                className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full dark:bg-indigo-50 bg-indigo-50 text-white flex-shrink-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#6366f1" className="bi bi-dice-5-fill" viewBox="0 0 16 16">
+                                    <path d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3zm2.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M12 13.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M8 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
+                                </svg>
+                            </div>
+                            <h2 className="text-white dark:text-white text-lg font-medium">{title}</h2>
                         </div>
-                        <h2 className="text-white dark:text-white text-lg font-medium">{title}</h2>
-                    </div>
-                    <div className="flex flex-col justify-between flex-grow">
-                        <p className="text-sm text-base text-white dark:text-gray-300">
-                            {description}
-                        </p>
+                        <div className="flex flex-col justify-between flex-grow">
+                            <p className="text-sm text-base text-white dark:text-white">
+                                {description}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/app/components/Games.js
+++ src/app/components/Games.js
@@ -4,51 +4,51 @@ import Game from "./Game"
 export default function Games() {
     return (
         <>
-            <div className="grid grid-rows-3 grid-flow-col justify-center gap-2">
+            <div className="container mx-auto mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4">
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titcrre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="ticestre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titcfsre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titre" />
                 <Game
                     title="titre"
                     description="description"
-                    image="logo.webp"
+                    image="game.jpg"
                     link="titre" />
             </div>
         </>
